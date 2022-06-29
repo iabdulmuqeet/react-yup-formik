97df583d72d350f6af5ce9e715cef3181df155c6
Abdul Muqeet Arshad <abdulmuqeet.arshad@devsinc.com>
2022-06-29 12:38:10 +0500

    Added floating input

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Formik } from 'formik';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, FloatingLabel } from 'react-bootstrap';
 
 import { schema } from './schema';
 
@@ -18,52 +18,54 @@ const SignupForm = () => {
 			{({ handleSubmit, handleChange, values, touched, isValid, errors }) => (
 				<Form noValidate onSubmit={handleSubmit}>
 					<Form.Group className='mb-3'>
-						<Form.Label>First Name</Form.Label>
-
-						<Form.Control
-							required
-							type='text'
-							name='firstName'
-							value={values.firstName}
-							onChange={handleChange}
-							placeholder='Enter First Name'
-							isInvalid={!!errors.firstName}
-							isValid={touched.firstName && !errors.firstName}
-						/>
+						<FloatingLabel controlId='floatingInput' label='First Name'>
+							<Form.Control
+								required
+								type='text'
+								name='firstName'
+								value={values.firstName}
+								onChange={handleChange}
+								placeholder='Enter First Name'
+								isInvalid={!!errors.firstName}
+								isValid={touched.firstName && !errors.firstName}
+							/>
+						</FloatingLabel>
 						<Form.Control.Feedback type='invalid'>
 							{errors.firstName}
 						</Form.Control.Feedback>
 					</Form.Group>
 
 					<Form.Group className='mb-3'>
-						<Form.Label>Last Name</Form.Label>
-						<Form.Control
-							required
-							type='text'
-							name='lastName'
-							value={values.lastName}
-							onChange={handleChange}
-							placeholder='Enter Last Name'
-							isInvalid={!!errors.lastName}
-							isValid={touched.lastName && !errors.lastName}
-						/>
+						<FloatingLabel controlId='floatingInput' label='Last Name'>
+							<Form.Control
+								required
+								type='text'
+								name='lastName'
+								value={values.lastName}
+								onChange={handleChange}
+								placeholder='Enter Last Name'
+								isInvalid={!!errors.lastName}
+								isValid={touched.lastName && !errors.lastName}
+							/>
+						</FloatingLabel>
 						<Form.Control.Feedback type='invalid'>
 							{errors.lastName}
 						</Form.Control.Feedback>
 					</Form.Group>
 
 					<Form.Group className='mb-3'>
-						<Form.Label>Email address</Form.Label>
-						<Form.Control
-							required
-							type='email'
-							name='email'
-							value={values.email}
-							onChange={handleChange}
-							placeholder='Enter email'
-							isInvalid={!!errors.email}
-							isValid={touched.email && !errors.email}
-						/>
+						<FloatingLabel controlId='floatingInput' label='Email'>
+							<Form.Control
+								required
+								type='email'
+								name='email'
+								value={values.email}
+								onChange={handleChange}
+								placeholder='Enter email'
+								isInvalid={!!errors.email}
+								isValid={touched.email && !errors.email}
+							/>
+						</FloatingLabel>
 						<Form.Control.Feedback type='invalid'>
 							{errors.email}
 						</Form.Control.Feedback>
